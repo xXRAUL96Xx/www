15f068c8a8a5aedda6fd63133a817f0afa901a89
Raul <xxraul96xx@outlook.es>
2017-02-24 09:46:36 +0100

    Initial commit Raul

---
+++ app/src/main/java/com/rauluem/www/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rauluem.www;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+    }
+}
